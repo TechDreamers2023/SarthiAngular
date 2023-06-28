8cadcc6258807bd72b122aa384ea4c5890380966
Kalpesh Sharma <kalpesh.sharma@radixweb.local>
2023-06-28 15:55:03 +0530

    Added Condition on customer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -45,162 +45,156 @@ export type ChartOptions = {
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-   styleUrls: ['./customer.component.scss']
+  styleUrls: ['./customer.component.scss']
 })
 
 export class CustomerComponent {
   navCollapsed: boolean;
   navCollapsedMob: boolean;
-// private props
-@ViewChild('growthChart') growthChart: ChartComponent;
-chartOptions: Partial<ChartOptions>;
-@ViewChild('bajajchart') bajajchart: ChartComponent;
-chartOptions1: Partial<ChartOptions>;
-monthChart: any;
-yearChart: any;
-colorChart = ['#673ab7'];
-fromvalue : PlaceSearchResult | undefined;
-tovalue : PlaceSearchResult | undefined;
+  // private props
+  @ViewChild('growthChart') growthChart: ChartComponent;
+  chartOptions: Partial<ChartOptions>;
+  @ViewChild('bajajchart') bajajchart: ChartComponent;
+  chartOptions1: Partial<ChartOptions>;
+  monthChart: any;
+  yearChart: any;
+  colorChart = ['#673ab7'];
+  fromvalue: PlaceSearchResult | undefined;
+  tovalue: PlaceSearchResult | undefined;
   windowWidth: number;
 
-// Constructor
-constructor(private zone: NgZone, private location: Location, 
-  private locationStrategy: LocationStrategy,
-  private customerService: CustomerService,
-  private toastr: ToastrService) {
+  // Constructor
+  constructor(private zone: NgZone, private location: Location,
+    private locationStrategy: LocationStrategy,
+    private customerService: CustomerService,
+    private toastr: ToastrService) {
 
-  let current_url = this.location.path();
-  if (this.location['_baseHref']) {
-    current_url = this.location['_baseHref'] + this.location.path();
-  }
+    let current_url = this.location.path();
+    if (this.location['_baseHref']) {
+      current_url = this.location['_baseHref'] + this.location.path();
+    }
 
-  if (current_url === this.location['_baseHref'] + '/layout/theme-compact' || current_url === this.location['_baseHref'] + '/layout/box')
-    this.windowWidth = window.innerWidth;
-  this.navCollapsed = this.windowWidth >= 1025 ? BerryConfig.isCollapse_menu : false;
-  this.navCollapsedMob = false;
-  this.chartOptions = {
-    series: [
-      {
-        name: 'Investment',
-        data: [35, 125, 35, 35, 35, 80, 35, 20, 35, 45, 15, 75]
+    if (current_url === this.location['_baseHref'] + '/layout/theme-compact' || current_url === this.location['_baseHref'] + '/layout/box')
+      this.windowWidth = window.innerWidth;
+    this.navCollapsed = this.windowWidth >= 1025 ? BerryConfig.isCollapse_menu : false;
+    this.navCollapsedMob = false;
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Investment',
+          data: [35, 125, 35, 35, 35, 80, 35, 20, 35, 45, 15, 75]
+        },
+        {
+          name: 'Loss',
+          data: [35, 15, 15, 35, 65, 40, 80, 25, 15, 85, 25, 75]
+        },
+        {
+          name: 'Profit',
+          data: [35, 145, 35, 35, 20, 105, 100, 10, 65, 45, 30, 10]
+        },
+        {
+          name: 'Maintenance',
+          data: [0, 0, 75, 0, 0, 115, 0, 0, 0, 0, 150, 0]
+        }
+      ],
+      dataLabels: {
+        enabled: false
       },
-      {
-        name: 'Loss',
-        data: [35, 15, 15, 35, 65, 40, 80, 25, 15, 85, 25, 75]
+      chart: {
+        type: 'bar',
+        height: 480,
+        stacked: true,
+        toolbar: {
+          show: true
+        }
       },
-      {
-        name: 'Profit',
-        data: [35, 145, 35, 35, 20, 105, 100, 10, 65, 45, 30, 10]
-      },
-      {
-        name: 'Maintenance',
-        data: [0, 0, 75, 0, 0, 115, 0, 0, 0, 0, 150, 0]
-      }
-    ],
-    dataLabels: {
-      enabled: false
-    },
-    chart: {
-      type: 'bar',
-      height: 480,
-      stacked: true,
-      toolbar: {
-        show: true
-      }
-    },
-    colors: ['#90caf9', '#1e88e5', '#673ab7', '#ede7f6'],
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          legend: {
-            position: 'bottom',
-            offsetX: -10,
-            offsetY: 0
+      colors: ['#90caf9', '#1e88e5', '#673ab7', '#ede7f6'],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: 'bottom',
+              offsetX: -10,
+              offsetY: 0
+            }
           }
         }
+      ],
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '50%'
+        }
+      },
+      xaxis: {
+        type: 'category',
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      grid: {
+        strokeDashArray: 4
+      },
+      tooltip: {
+        theme: 'dark'
       }
-    ],
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: '50%'
-      }
-    },
-    xaxis: {
-      type: 'category',
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    grid: {
-      strokeDashArray: 4
-    },
-    tooltip: {
-      theme: 'dark'
-    }
-  };
-  this.chartOptions1 = {
-    chart: {
-      type: 'area',
-      height: 95,
-      stacked: true,
-      sparkline: {
-        enabled: true
-      }
-    },
-    colors: ['#673ab7'],
-    stroke: {
-      curve: 'smooth',
-      width: 1
-    },
+    };
+    this.chartOptions1 = {
+      chart: {
+        type: 'area',
+        height: 95,
+        stacked: true,
+        sparkline: {
+          enabled: true
+        }
+      },
+      colors: ['#673ab7'],
+      stroke: {
+        curve: 'smooth',
+        width: 1
+      },
 
-    series: [
-      {
-        data: [0, 15, 10, 50, 30, 40, 25]
-      }
-    ]
-  };
-} 
+      series: [
+        {
+          data: [0, 15, 10, 50, 30, 40, 25]
+        }
+      ]
+    };
+  }
 
-timerSubscription!: Subscription; 
+  timerSubscription!: any;
 
-ngOnInit(): void {
+  ngOnInit() {
+    this.getCurrentRequestStatus(1);
+    // this.timerSubscription = timer(0, 20000).subscribe((res) => {
+    //   if (res) {
+    //     this.getCurrentRequestStatus(1); 
+    //   }
+    // }); 
+  }
 
-  this.timerSubscription = Observable.timer(0, 1000).pipe( map(() => {  this.getCurrentRequestStatus(1); 
-     
-}
-  ))}
- 
-ngOnDestroy(): void { this.timerSubscription.unsubscribe(); } 
+  ngOnDestroy() { this.timerSubscription.unsubscribe(); }
 
-responces: any;
-  getCurrentRequestStatus(customerId : number) {
+  responces: any;
+  showLocationFilter: boolean = true;
+
+  getCurrentRequestStatus(customerId: number) {
     this.customerService.GetCurrentStatusByCustomer(customerId)
       .subscribe(
-        (response) => {       
-          debugger;                     
+        (response) => {
           this.responces = response;
-          console.log(response);
-          // if (this.responces.status == 1) {
-          //   if (this.responces.data[0].usertypeId === 2 ||
-          //     this.responces.data[0].usertypeId ===  4) {
-          //     this.toastr.success(response.message)
-          //     localStorage.setItem("UserID", this.responces.data[0].userId);
-          //     localStorage.setItem("UserTypeID", this.responces.data[0].usertypeId);
-          //     localStorage.setItem("User", this.responces.data[0].firstName);
-          //     localStorage.setItem("UserName", this.responces.data[0].firstName + " " + this.responces.data[0].firstName);
-          //     if(this.responces.data[0].usertypeId === 2)
-          //     {
-          //     this.router.navigate(['/customer']);
-          //     }
-          //     else if(this.responces.data[0].usertypeId === 4)
-          //     {
-          //       this.router.navigate(['/vendor']);
-          //     }
-          //   }
-          // }
-          // if (this.responces.status == 0 || this.responces.status == 2) {
-          //   this.toastr.error(response.message)
-          // }
+          console.log(this.responces);
+          if (this.responces.status == 1) {
+            if (this.responces.data.currentStageId < 8) {
+              this.showLocationFilter = false;
+              console.log(this.showLocationFilter);
+            }
+            else {
+              this.showLocationFilter = true;
+            }
+          }
+          if (this.responces.status == 0 || this.responces.status == 2) {
+            this.toastr.error(response.message)
+          }
         },
         (error) => {
           this.toastr.error("Something went wrong, Please try Again ")                    //error() callback
@@ -210,169 +204,169 @@ responces: any;
           console.log("Completed")
         })
   }
-// public Method
-onNavChange(changeEvent: NgbNavChangeEvent) {
-  if (changeEvent.nextId === 1) {
-    setTimeout(() => {
-      this.monthChart = new ApexCharts(document.querySelector('#tab-chart-1'), this.monthOptions);
-      this.monthChart.render();
-    }, 200);
-  }
-
-  if (changeEvent.nextId === 2) {
-    setTimeout(() => {
-      this.yearChart = new ApexCharts(document.querySelector('#tab-chart-2'), this.yearOptions);
-      this.yearChart.render();
-    }, 200);
-  }
-}
-
-ListGroup = [
-  {
-    name: 'Bajaj Finery',
-    profit: '10% Profit',
-    invest: '$1839.00',
-    bgColor: 'bg-light-success',
-    icon: 'ti ti-chevron-up',
-    color: 'text-success'
-  },
-  {
-    name: 'TTML',
-    profit: '10% Loss',
-    invest: '$100.00',
-    bgColor: 'bg-light-danger',
-    icon: 'ti ti-chevron-down',
-    color: 'text-danger'
-  },
-  {
-    name: 'Reliance',
-    profit: '10% Profit',
-    invest: '$200.00',
-    bgColor: 'bg-light-success',
-    icon: 'ti ti-chevron-up',
-    color: 'text-success'
-  },
-  {
-    name: 'ATGL',
-    profit: '10% Loss',
-    invest: '$189.00',
-    bgColor: 'bg-light-danger',
-    icon: 'ti ti-chevron-down',
-    color: 'text-danger'
-  },
-  {
-    name: 'Stolon',
-    profit: '10% Profit',
-    invest: '$210.00',
-    bgColor: 'bg-light-success',
-    icon: 'ti ti-chevron-up',
-    color: 'text-success'
-  }
-];
-
-monthOptions = {
-  chart: {
-    type: 'line',
-    height: 90,
-    sparkline: {
-      enabled: true
+  // public Method
+  onNavChange(changeEvent: NgbNavChangeEvent) {
+    if (changeEvent.nextId === 1) {
+      setTimeout(() => {
+        this.monthChart = new ApexCharts(document.querySelector('#tab-chart-1'), this.monthOptions);
+        this.monthChart.render();
+      }, 200);
     }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  colors: ['#FFF'],
-  stroke: {
-    curve: 'smooth',
-    width: 3
-  },
-  series: [
+
+    if (changeEvent.nextId === 2) {
+      setTimeout(() => {
+        this.yearChart = new ApexCharts(document.querySelector('#tab-chart-2'), this.yearOptions);
+        this.yearChart.render();
+      }, 200);
+    }
+  }
+
+  ListGroup = [
     {
-      name: 'series1',
-      data: [45, 66, 41, 89, 25, 44, 9, 54]
+      name: 'Bajaj Finery',
+      profit: '10% Profit',
+      invest: '$1839.00',
+      bgColor: 'bg-light-success',
+      icon: 'ti ti-chevron-up',
+      color: 'text-success'
+    },
+    {
+      name: 'TTML',
+      profit: '10% Loss',
+      invest: '$100.00',
+      bgColor: 'bg-light-danger',
+      icon: 'ti ti-chevron-down',
+      color: 'text-danger'
+    },
+    {
+      name: 'Reliance',
+      profit: '10% Profit',
+      invest: '$200.00',
+      bgColor: 'bg-light-success',
+      icon: 'ti ti-chevron-up',
+      color: 'text-success'
+    },
+    {
+      name: 'ATGL',
+      profit: '10% Loss',
+      invest: '$189.00',
+      bgColor: 'bg-light-danger',
+      icon: 'ti ti-chevron-down',
+      color: 'text-danger'
+    },
+    {
+      name: 'Stolon',
+      profit: '10% Profit',
+      invest: '$210.00',
+      bgColor: 'bg-light-success',
+      icon: 'ti ti-chevron-up',
+      color: 'text-success'
     }
-  ],
-  yaxis: {
-    min: 5,
-    max: 95
-  },
-  tooltip: {
-    theme: 'dark',
-    fixed: {
-      enabled: false
-    },
-    x: {
-      show: false
-    },
-    y: {
-      title: {
-        formatter: function (seriesName) {
-          return 'Total Earning';
-        }
+  ];
+
+  monthOptions = {
+    chart: {
+      type: 'line',
+      height: 90,
+      sparkline: {
+        enabled: true
       }
     },
-    marker: {
-      show: false
-    }
-  }
-};
-
-yearOptions = {
-  chart: {
-    type: 'line',
-    height: 90,
-    sparkline: {
-      enabled: true
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  colors: ['#FFF'],
-  stroke: {
-    curve: 'smooth',
-    width: 3
-  },
-  series: [
-    {
-      name: 'series1',
-      data: [35, 44, 9, 54, 45, 66, 41, 69]
-    }
-  ],
-  yaxis: {
-    min: 5,
-    max: 95
-  },
-  tooltip: {
-    theme: 'dark',
-    fixed: {
+    dataLabels: {
       enabled: false
     },
-    x: {
-      show: false
+    colors: ['#FFF'],
+    stroke: {
+      curve: 'smooth',
+      width: 3
     },
-    y: {
-      title: {
-        formatter: function (seriesName) {
-          return 'Total Earning';
+    series: [
+      {
+        name: 'series1',
+        data: [45, 66, 41, 89, 25, 44, 9, 54]
+      }
+    ],
+    yaxis: {
+      min: 5,
+      max: 95
+    },
+    tooltip: {
+      theme: 'dark',
+      fixed: {
+        enabled: false
+      },
+      x: {
+        show: false
+      },
+      y: {
+        title: {
+          formatter: function (seriesName) {
+            return 'Total Earning';
+          }
         }
+      },
+      marker: {
+        show: false
+      }
+    }
+  };
+
+  yearOptions = {
+    chart: {
+      type: 'line',
+      height: 90,
+      sparkline: {
+        enabled: true
       }
     },
-    marker: {
-      show: false
+    dataLabels: {
+      enabled: false
+    },
+    colors: ['#FFF'],
+    stroke: {
+      curve: 'smooth',
+      width: 3
+    },
+    series: [
+      {
+        name: 'series1',
+        data: [35, 44, 9, 54, 45, 66, 41, 69]
+      }
+    ],
+    yaxis: {
+      min: 5,
+      max: 95
+    },
+    tooltip: {
+      theme: 'dark',
+      fixed: {
+        enabled: false
+      },
+      x: {
+        show: false
+      },
+      y: {
+        title: {
+          formatter: function (seriesName) {
+            return 'Total Earning';
+          }
+        }
+      },
+      marker: {
+        show: false
+      }
     }
-  }
-};
+  };
 
-// public method
-navMobClick() {
-  if (this.navCollapsedMob && (document.querySelector('app-navigation.coded-navbar') as HTMLDivElement).classList.contains('mob-open')) {
-    this.navCollapsedMob = !this.navCollapsedMob;
-    setTimeout(() => {
+  // public method
+  navMobClick() {
+    if (this.navCollapsedMob && (document.querySelector('app-navigation.coded-navbar') as HTMLDivElement).classList.contains('mob-open')) {
       this.navCollapsedMob = !this.navCollapsedMob;
-    }, 100);
-  } else {
-    this.navCollapsedMob = !this.navCollapsedMob;
+      setTimeout(() => {
+        this.navCollapsedMob = !this.navCollapsedMob;
+      }, 100);
+    } else {
+      this.navCollapsedMob = !this.navCollapsedMob;
+    }
   }
-}
 }
